e7aa99a5edf4537150390d0b673a81f5831671e1
t0t <sergiofores@gmail.com>
2021-10-21 14:36:37 +0200

    ajustes layout

--- _includes/switcher-3d-scroll.js
+++ _includes/switcher-3d-scroll.js
@@ -3,24 +3,24 @@ const canvasElement = document.querySelector("model-viewer");
 
 // selecciono elementos del switcher
 const icono = document.getElementById("icono-switcher");
-// let innerTxtSwitch = document.getElementById("idSwitch");
+let innerTxtSwitch = document.getElementById("idSwitch");
 const boton = document.getElementById("btn-mover");
 
 // Escucho los eventos
-// boton.addEventListener("click", activarControl, true);
-// boton.addEventListener("touchstart", activarControl, true);
+boton.addEventListener("click", activarControl, true);
+boton.addEventListener("touchstart", activarControl, true);
 
 // Texto indicador de accion
-// innerTxtSwitch.innerText = "ACTIVAR controles 3D";
+innerTxtSwitch.innerText = "ACTIVAR controles 3D";
 
-// function activarControl() {
-//   if (boton.checked) {
-//     innerTxtSwitch.innerText = "Controles 3D ACTIVOS";
-//     canvasElement.classList.add("deshabilitar");
-//     icono.classList.add("indicador-icono");
-//   } else if (!boton.checked){
-//     innerTxtSwitch.innerText = "ACTIVAR controles 3D";
-//     canvasElement.classList.remove("deshabilitar");
-//     icono.classList.remove("indicador-icono");
-//   }
-// }
+function activarControl() {
+  if (boton.checked) {
+    innerTxtSwitch.innerText = "Controles 3D ACTIVOS";
+    canvasElement.classList.add("deshabilitar");
+    icono.classList.add("indicador-icono");
+  } else if (!boton.checked){
+    innerTxtSwitch.innerText = "ACTIVAR controles 3D";
+    canvasElement.classList.remove("deshabilitar");
+    icono.classList.remove("indicador-icono");
+  }
+}
