f7220ba2fe9d034772d709f7396b1437a10e03cd
t0t <sergiofores@gmail.com>
2021-10-22 15:01:12 +0200

    implementado un loader al sitio

--- js/main.js
+++ js/main.js
@@ -1,15 +1,40 @@
-const timmy = 12412423414141;
-console.log("cargado");
 
-document.addEventListener("DOMContentLoaded", () => {
-  two();
-  document.querySelector("footer").addEventListener("click", one)
+
+let loader = document.getElementById("wrap-preloader");
+let theSite = document.getElementById("theSite");
+
+window.addEventListener("load", () =>  {
+  loader.style.display = "none";
+  theSite.style.display = "inherit";
+  fundidoPagina();
 });
 
-let one = () => {
-  console.log("Website desarrollada a mano y con mucho cariño")
+let fundidoPagina = () => {
+  document.querySelector("#theSite").classList.add("fade-page-on")
+  console.log("Web cargada sin errores");
 }
 
-let two = () => {
-  console.log("By Sergio Forés")
-}
+// Page Preload y fade page
+document.addEventListener("DOMContentLoaded", () => {
+
+  console.log(loader);
+
+  // showPage();
+  // document.querySelector("body").addEventListener("click", credits)
+});
+
+
+// let credits = () => {
+//   console.log("Website desarrollada a mano y con mucho cariño por Sergio Forés")
+// }
+
+
+
+// function myFunction() {
+//   myVar = setTimeout(showPage, 3000);
+// }
+
+// function showPage() {
+//   preLoader.style.display = "none";
+//   document.getElementById("theSite").style.display = "block";
+// }
