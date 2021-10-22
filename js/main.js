4629a4fcd4a0ebbf7acbd86846cb40f8283a4a27
t0t <sergiofores@gmail.com>
2021-10-22 12:28:14 +0200

    anadida fuente montserrat

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("Code is poetry");
+const timmy = 12412423414141;
+console.log("cargado");
+
+document.addEventListener("DOMContentLoaded", () => {
+  two();
+  document.querySelector("footer").addEventListener("click", one)
+});
+
+let one = () => {
+  console.log("Website desarrollada a mano y con mucho cariño")
+}
+
+let two = () => {
+  console.log("By Sergio Forés")
+}
