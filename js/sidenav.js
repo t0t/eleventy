5495b247093f81359bc37f4a99f8738799c92289
t0t <sergiofores@gmail.com>
2021-10-22 23:00:47 +0200

    sidenav

--- js/sidenav.js
+++ js/sidenav.js
@@ -1,9 +1,13 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "50vw";
   document.getElementById("theSite").style.marginRight = "50vw";
+  document.getElementById("maincontent").style.opacity = "0.3";
+  document.body.style.overflow = "hidden";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("theSite").style.marginRight= "0";
+  document.getElementById("maincontent").style.opacity= "inherit";
+  document.body.style.overflow = "inherit";
 }
