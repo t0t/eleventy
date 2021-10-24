c3efc8a8efe6e404d5f939ad3389ae4633869b31
t0t <sergiofores@gmail.com>
2021-10-24 16:24:01 +0200

    layout bug

--- js/svg.js
+++ js/svg.js
@@ -1,10 +1,12 @@
 let puntero =document.getElementsByClassName("puntero");
 console.log(puntero);
 
-
+// svg animacion metodo
 let metodoSVG = document.querySelector("svg#metodo");
-
 if (metodoSVG) {
   metodoSVG.width = window.innerWidth;
   metodoSVG.height = window.outerHeight;
 }
+
+let coversvg = document.getElementById("coversvg");
+// coversvg.width = "100%"
