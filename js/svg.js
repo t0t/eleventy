109952e758b27edf25643c760df85a1d0f7be278
t0t <sergiofores@gmail.com>
2021-10-26 13:43:37 +0200

    js interaction

--- js/svg.js
+++ js/svg.js
@@ -1,51 +1,32 @@
 function load() {
 
   // svg animacion metodo
-  let metodoSVG = document.querySelector("#metodosvg");
+  let metodoSVG = document.querySelector("svg#metodosvg");
 
 
   if (metodoSVG) {
-    metodoSVG.width = window.innerWidth;
-    metodoSVG.height = window.outerHeight;
+    metodoSVG.style.width = window.innerWidth;
+    // metodoSVG.style.height = window.innerHeight/2;
+    console.log(metodoSVG.width);
 
-    let elementoCero = document.querySelector("svg#metodosvg circle#cero");
-    let elementoUno = document.querySelector("svg#metodosvg circle#uno");
-    let elementoDos = document.querySelector("svg#metodosvg circle#dos");
-    let elementoTres = document.querySelector("svg#metodosvg circle#tres");
-    let elementoCuatro = document.querySelector("svg#metodosvg circle#cuatro");
+    const svgBtns = document.querySelectorAll(".clickableItem");
 
-    elementoCero.addEventListener("click", accionCero);
-    elementoUno.addEventListener("click", accionUno);
-    elementoDos.addEventListener("click", accionDos);
-    elementoTres.addEventListener("click", accionTres);
-    elementoCuatro.addEventListener("click", accionCuatro);
-  }
+    svgBtns.forEach((svgBtn,idx) => {
+      svgBtn.addEventListener("click", () => {
+        ToggleActive(svgBtn,idx);
+      });
+    });
 
+    function ToggleActive(el,index) {
+      el.classList.toggle("txt--active");
+      svgBtns.forEach((svgBtn,idx) => {
+        if(idx !== index) {
+          svgBtn.classList.remove("txt--active");
+        }
+      });
+    }
 
-
-  function accionCero(e) {
-    let txtCero = document.getElementById("txt0");
-    txtCero.classList.toggle("txt--active");
-  }
-
-  function accionUno(e) {
-    let txtUno = document.getElementById("txt1");
-    txtUno.classList.toggle("txt--active");
-  }
-
-  function accionDos(e) {
-    let txtDos = document.getElementById("txt2");
-    txtDos.classList.toggle("txt--active");
-  }
-
-  function accionTres(e) {
-    let txtTres = document.getElementById("txt3");
-    txtTres.classList.toggle("txt--active");
-  }
-
-  function accionCuatro(e) {
-    let txtCuatro = document.getElementById("txt4");
-    txtCuatro.classList.toggle("txt--active");
+    console.log(svgBtns);
   }
 
 
