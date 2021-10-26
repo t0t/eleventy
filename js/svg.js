f4b2c187baa383d10f82a2efeab3ea5edc181e53
t0t <sergiofores@gmail.com>
2021-10-26 20:32:23 +0200

    refactorizado css

--- js/svg.js
+++ js/svg.js
@@ -1,41 +1,37 @@
 function load() {
-
   // svg animacion metodo
-  let metodoSVG = document.querySelector("svg#metodosvg");
-
+  const metodoSVG = document.querySelector("svg#metodosvg");
+  const svgBtns = document.querySelectorAll(".clickableItem");
+  const svgTxts = document.querySelectorAll(".txt");
 
   if (metodoSVG) {
-    metodoSVG.style.width = window.innerWidth;
-    metodoSVG.style.height = window.innerHeight;
 
-    const svgBtns = document.querySelectorAll(".clickableItem");
-    const svgTxts = document.querySelectorAll(".txt");
+    var onresize = function() {
+      metodoSVG.style.width = window.innerWidth;
+      // metodoSVG.style.height = window.innerHeight;
+      // width = document.body.clientWidth;
+      // height = document.body.clientHeight;
+   }
+   window.addEventListener("resize", onresize);
 
-    // svgTxts.forEach(svgTxt => {
-    //   console.log(svgTxt);
-    // });
 
-    svgBtns.forEach((svgBtn,idx) => {
+    svgBtns.forEach((svgBtn, idx) => {
       svgBtn.addEventListener("click", () => {
-        ToggleActive(svgBtn,idx);
-        svgTxts[idx].style.display = "block";
-        console.log(svgTxts[idx]);
+        ToggleActive(svgBtn, idx);
       });
     });
 
-    function ToggleActive(el,index) {
+    function ToggleActive(el, index) {
       el.classList.add("txt--active");
-      svgBtns.forEach((svgBtn,idx) => {
-        if(idx !== index) {
+      svgTxts[index].style.display = "block";
+      svgBtns.forEach((svgBtn, idx) => {
+        if (idx !== index) {
+          svgTxts[idx].style.display = "none";
           svgBtn.classList.remove("txt--active");
         }
       });
     }
-
   }
-
-
-
 }
 
 document.addEventListener("DOMContentLoaded", load, false);
