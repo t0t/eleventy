0925408e85168d34f955ebc48e7aaca9be15bafa
t0t <sergiofores@gmail.com>
2021-11-05 01:27:06 +0100

    D3

--- js/animintro.js
+++ js/animintro.js
@@ -1,32 +1,36 @@
 // scale
+let width = window.innerWidth;
+let height = window.innerHeight;
 
-const svg = d3.select("#animcover"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    angles = d3.range(0, 2 * Math.PI, Math.PI / 200);
+let svg = d3.select("#animcover"),
+angles = d3.range(0, 2 * Math.PI, Math.PI / 100);
+
+svg.attr("width", width)
+   .attr("height", height)
+   .attr("fill", "#00ff00");
 
 const path = svg.append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .attr("transform", `translate(${width/2},${height/2})`)
     .attr("fill", "none")
-    .attr("stroke-width", 4)
+    .attr("stroke-width", 30)
     .attr("stroke-linejoin", "round")
   .selectAll("path")
   .data(["#9F9FFF", "#FF6874", "#FFFF9F", "#2BC4A9"])
   .enter().append("path")
     .attr("stroke", d => { return d; })
-    .style("mix-blend-mode", "darken")
-    .datum(function(d, i) {
+    .style("mix-blend-mode", "multiply")
+    .datum((d, i) => {
       return d3.radialLine()
           .curve(d3.curveLinearClosed)
-          .angle(function(a) { return a; })
-          .radius(function(a) {
+          .angle(a => { return a; })
+          .radius(a => {
             var t = d3.now() / 1000;
-            return 200 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 3) * 32;
+            return 150 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 10) * 32;
           });
     });
 
 d3.timer( () => {
-  path.attr("d", (d) => {
+  path.attr("d", d => {
     return d(angles);
   });
 });
